954a2d3e6b3d41c85989d4617ce5250d94ba3cf8
Emiliyan Stefkov <93265496+EStefkov@users.noreply.github.com>
2024-04-11 09:37:42 +0300

    Pushing

---
+++ example.py
@@ -0,0 +1,99 @@
+#https://colab.research.google.com/
+
+import numpy as np
+from random import randint
+from sklearn.utils import shuffle
+from sklearn.preprocessing import MinMaxScaler
+%matplotlib inline
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
+
+train_samples =[]
+train_labels = []
+
+for i in range(50):
+  random_younger = randint(13,64)
+  train_samples.append(random_younger)
+  train_labels.append(1)
+    
+  random_older = randint(65,100)
+  train_samples.append(random_older)
+  train_labels.append(0)
+
+for i in range(1000):
+  random_younger = randint(13,64)
+  train_samples.append(random_younger)
+  train_labels.append(0)
+
+  random_older = randint(65,100)
+  train_samples.append(random_older)
+  train_labels.append(1)
+
+'''
+for i in train_samples:
+  print(i)
+
+for i in train_labels:
+  print(i)
+  '''
+train_labels = np.array(train_labels)
+
+train_samples = np.array(train_samples)
+
+train_labels, train_samples = shuffle(train_labels, train_samples)
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_train_samples = scaler.fit_transform(train_samples.reshape(-1,1))
+
+predictions = model.predict(
+    x= scaled_train_samples,
+    batch_size=10,
+    verbose=0
+)
+
+'''for i in predictions:
+  print(i)
+
+
+ 
+'''
+rounded_predictions= np.argmax(predictions, axis=-1)
+
+cm = confusion_matrix( y_true = train_labels , y_pred=rounded_predictions)
+
+
+def plot_confusion_matrix(cm,
+                          classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+  plt.imshow(cm, interpolation='nearest', cmap=cmap)
+  plt.title(title)
+  plt.colorbar()
+  tick_marks = np.arange(len(classes))
+  plt.xticks(tick_marks,classes,rotation=45)
+  plt.yticks(tick_marks,classes)
+
+  if normalize:
+    cm=cm.astype('float') / cm.sum(axis=1)[:,np.newaxis]
+    print("Normalized confusion matrix")
+  else:
+    print('Confusion matrix, without normalization')
+
+  print(cm)
+  thresh = cm.max()/2.
+  for i,j in itertools.product(range(cm.shape[0]),range(cm.shape[1])):
+    plt.text(j,i,cm[i,j],
+    horizontalalignment = "center",
+    color="white" if cm[i,j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+  cm_plot_labels = ['без-странични ефекти','със странични-ефекти']
+
+plot_confusion_matrix(cm=cm, classes= cm_plot_labels, title ='Confusion Matrix')
+
+                        
